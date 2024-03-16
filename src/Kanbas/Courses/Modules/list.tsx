b58040e85352d961e0bcb4ee00527e2ea05b8d07
tphine <tristanphan7@gmail.com>
2024-03-16 16:32:32 -0400

    Modules update

--- src/Kanbas/Courses/Modules/list.tsx
+++ src/Kanbas/Courses/Modules/list.tsx
@@ -3,13 +3,24 @@ import "./index.css";
 import { modules } from "../../Database";
 import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaRegCheckCircle } from "react-icons/fa";
 import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import {
+    addModule,
+    deleteModule,
+    updateModule,
+    setModule,
+} from "./reducer";
+import { KanbasState } from "../../store";
 function ModuleList() {
     const { courseId } = useParams();
-    const modulesList = modules.filter((module) => module.course === courseId);
-    const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+    const moduleList = useSelector((state: KanbasState) =>
+        state.modulesReducer.modules);
+    const module = useSelector((state: KanbasState) =>
+        state.modulesReducer.module);
+    const dispatch = useDispatch();
     return (
-        <div className="col" style={{overflow: "hidden"}}>
-            <div className="right-align" style={{overflow: "hidden"}}>
+        <div className="col" style={{ overflow: "hidden" }}>
+            <div className="right-align" style={{ overflow: "hidden" }}>
                 <button className="default-btn">Collapse All</button>
                 <button className="default-btn">View Progress</button>
                 <select className="default-btn" id="publish-all">
@@ -17,23 +28,55 @@ function ModuleList() {
                 </select>
                 <button className="default-btn module-btn">+ Module</button>
             </div>
-            <ul className="list-group wd-modules" style={{overflow: "hidden"}}>
-                {modulesList.map((module) => (
-                    <li
-                        className="list-group-item"
-                        onClick={() => setSelectedModule(module)}>
-                        <div>
-                            <FaEllipsisV className="me-2" />
-                            {module.name}
-                            <span className="float-end">
-                                <FaCheckCircle className="text-success" />
-                                <FaPlusCircle className="ms-2" />
-                                <FaEllipsisV className="ms-2" />
-                            </span>
-                        </div>
-                        {selectedModule._id === module._id && (
+            <ul className="wd-modules" style={{ overflow: "hidden" }}>
+                <li className="new-module">
+                    <input value={module.name}
+                        onChange={(e) => dispatch(setModule({ ...module, name: e.target.value }))}
+                        className="module-input"
+                    />
+                    <button className="add-btn"
+                        onClick={() => dispatch(addModule({ ...module, course: courseId }))}>
+                        Add</button>
+                    <button
+                        className="update-btn"
+                        onClick={() => dispatch(updateModule(module))}>
+                        Update
+                    </button>
+
+                    <br />
+                    <textarea value={module.description}
+                        onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))}
+                        className="module-input"
+                    />
+                </li>
+
+                {moduleList
+                    .filter((module) => module.course === courseId)
+                    .map((module, index) => (
+                        <li
+                            className="list-group-item">
+                            <div>
+                                <FaEllipsisV className="me-2" />
+                                {module.name}
+                                <span className="float-end">
+                                    <button
+                                        onClick={() => dispatch(setModule(module))}
+                                        className="edit-btn">
+                                        Edit
+                                    </button>
+
+                                    <button
+                                         onClick={() => dispatch(deleteModule(module._id))}
+                                        className="delete-btn">
+                                        Delete
+                                    </button>
+                                    <FaCheckCircle className="text-success" />
+                                    <FaPlusCircle className="ms-2" />
+                                    <FaEllipsisV className="ms-2" />
+                                </span>
+                            </div>
                             <ul className="list-group">
-                                {module.lessons?.map((lesson) => (
+                                {module.lessons?.map((lesson: { _id: string, name: string, description: string, module: string }) => (
                                     <li className="list-group-item">
                                         <FaEllipsisV className="me-2" />
                                         {lesson.name}
@@ -44,9 +87,8 @@ function ModuleList() {
                                     </li>
                                 ))}
                             </ul>
-                        )}
-                    </li>
-                ))}
+                        </li>
+                    ))}
             </ul>
         </div>
     );
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,17 +1,20 @@
 import KanbasNavigation from "./Navigation";
 import NavigationHorizontal from "./Navigation/NavigationHorizontal";
 import routes from "./routes";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Kanbas() {
     return (
-        <div className="d-flex" style={{overflow: "hidden"}}>
-            <KanbasNavigation />
-            <div style={{ width: "100%" }}>
-                <NavigationHorizontal />
-                {routes}
+        <Provider store={store}>
+            <div className="d-flex" style={{ overflow: "hidden" }}>
+                <KanbasNavigation />
+                <div style={{ width: "100%" }}>
+                    <NavigationHorizontal />
+                    {routes}
+                </div>
             </div>
-
-        </div>
+        </Provider>
     );
 }
 export default Kanbas;
